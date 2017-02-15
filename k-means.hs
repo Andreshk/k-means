314621a8ed72f34462190188a732db469950746c
Andrey Drenski <andreshko.dre@gmail.com>
2017-02-15 22:12:24 +0200

    finished version

--- k-means.hs
+++ k-means.hs
@@ -1,65 +1,59 @@
 module Main (main) where
 
 import System.Environment (getArgs)
-import System.CPUTime (getCPUTime)
-import System.IO (writeFile)
+import System.Clock (TimeSpec, Clock(Realtime), getTime, diffTimeSpec, toNanoSecs) -- requires "clock" package
+import System.IO (readFile,writeFile,hFlush,stdout)
 import Data.List (foldl',minimumBy)
 import Data.Ord (comparing)
 
 type Point = (Double,Double)
-euclDist :: Point -> Point -> Double
-euclDist (a,b) (c,d) = (a-c)^2 + (b-d)^2
+dist :: Point -> Point -> Double
+dist (a,b) (c,d) = (a-c)^2 + (b-d)^2
 
-infixl 6 +. -- syntactic sugar, used only once :(
-(+.) :: Point -> Point -> Point
-(a,b) +. (c,d) = (a+c, b+d)
+-- Main heavy-lifting function, generating the initial centers and
+-- clusters and passing them to the looping nextIteration function.
+-- Returns the final clusters as well as the number of iterations needed.
+-- Each point is given an index in the range [1..k], showing to which cluster it belongs.
+clusterize :: Int -> [Point] -> ([[Point]], Int)
+clusterize k points = nextIteration k initialCenters initialClusters 1
+  where initialCenters = zip (take k points) [1..] --lol, needs changing
+        initialClusters = fst $ reClusterize initialCenters [(p,-1) | p<-points]
 
-data Cluster = Cluster [Point] Point Int
-instance Show Cluster where
-    show cl@(Cluster pts _ size) = "Cluster: " ++ show pts ++
-                        " {"++ show x ++ "," ++ show y ++ "} |" ++ show size ++ "|"
-      where (x,y) = clusterMean cl
+-- At every step this function calculates the new cluster centers,
+-- reclusterizes the points and breaks when no point has changed its cluster.
+nextIteration :: Int -> [(Point,Int)] -> [(Point,Int)] -> Int -> ([[Point]], Int)
+nextIteration k centers points iter
+  | isStable  = (groupToClusters k newpoints, iter)
+  | otherwise = nextIteration k newcenters newpoints (iter+1)
+  where newcenters = calculateMeans k points
+        (newpoints,isStable) = reClusterize newcenters points
 
-emptyCluster :: Cluster
-emptyCluster = Cluster [] (0,0) 0
+-- Groups the indexed points in simple lists by their cluster index.
+groupToClusters :: Int -> [(Point,Int)] -> [[Point]]
+groupToClusters k points = [ [ p | (p,j)<-points, i==j ] | i<-[1..k]]
 
-insertPoint :: Point -> Cluster -> Cluster
-insertPoint p (Cluster pts sum size) = Cluster (p:pts) (p+.sum) (size+1)
+-- Calculates all clusters' means by firstly grouping the indexed points by clusters.
+calculateMeans :: Int -> [(Point,Int)] -> [(Point,Int)]
+calculateMeans k points = zip (map mean $ groupToClusters k points) [1..]
+  where mean pts = let (xs,ys) = unzip pts
+                       len = fromIntegral $ length pts
+                   in (sum xs / len, sum ys / len)
 
-clusterMean :: Cluster -> Point
-clusterMean (Cluster _ (x,y) s) = let fs = fromIntegral s in (x/fs, y/fs)
+-- Recalculates the nearest center for every point, while keeping
+-- check whether a single point has changed its cluster. Done with
+-- a simple strict fold of the indexed point list.
+reClusterize :: [(Point,Int)] -> [(Point,Int)] -> ([(Point,Int)], Bool)
+reClusterize centers = foldl' (\(result,flag) (p,i) -> let newI = snd $ minimumBy (comparing (dist p . fst)) centers
+                                                       in ((p,newI):result, flag && newI == i)) ([],True)
 
-makeCluster :: [Point] -> Cluster
-makeCluster = foldl' (flip insertPoint) emptyCluster -- по-бързо от foldr
-
-indexPoints :: Int -> [Point] -> [(Point,Int)]
-indexPoints i pts = [ (p,i) | p<-pts ]
-
---- !!! heavy lifting goes here
-clusterize :: (Point -> Point -> Double) -> Int -> [Point] -> [Cluster]
-clusterize _ _ pts = [makeCluster pts] -- lol
-
-nextIteration :: (Point -> Point -> Double) -> Int -> [(Point,Int)] -> [[Point]] -> [[Point]]
-nextIteration dist k centers clusters = replicate k []
-  where indexedPts :: [(Point,Int)]
-        indexedPts = concat $ zipWith indexPoints [1..] clusters
-        nearestCenter :: (Point -> Point -> Double) -> Point -> [(Point,Int)] -> Int
-        nearestCenter dist p = snd . minimumBy (comparing (dist p . fst))
----- !!!
-
-printPoint :: Point -> String
-printPoint (x,y) = show x ++ " " ++ show y
-
+-- Small, useful IO-related functions
 readPoint :: String -> Point
 readPoint str = (read x, read y)
   where (x:y:_) = words str
 
-printSingleCluster :: Cluster -> String
-printSingleCluster (Cluster pts _ _) = unlines $ map printPoint pts
-
-printClusters :: [Cluster] -> String
-printClusters clusters = concat $ zipWith (\ cl idx  -> "Cluster " ++ show idx ++ ":\n"
-                                            ++ (printSingleCluster cl) ++ "\n\n") clusters [1..]
+printClusters :: [[Point]] -> String
+printClusters clusters = concat $ zipWith (\ cl idx  -> "Cluster " ++ show idx ++ ":\n" ++ (printCl cl) ++ "\n") clusters [1..]
+  where printCl = unlines . map (\(x,y) -> show x ++ " " ++ show y)
 
 -- impure Haskell = ugly Haskell
 -- ugly Haskell > ugly C++
@@ -70,11 +64,14 @@ main = do
     else do
         let (inFilename:k:outFilename:_) = args
         putStr $ "Clustering into " ++ k ++ " clusters... "
+        hFlush stdout
+        start <- getTime Realtime
         contents <- readFile inFilename
         let points = map readPoint $ lines contents
-        start <- getCPUTime
-        let clusters = clusterize euclDist (read k) $! points
-        end <- getCPUTime
+            (clusters, iterations) = clusterize (read k) points
         writeFile outFilename $ printClusters clusters
-        let diff = ((fromIntegral (end - start)) / (10^12)) :: Double
-        putStrLn $ "done. (" ++ show ((fromIntegral (end - start)) / (10^12)) ++ "sec)"
+        end <- getTime Realtime
+        putStrLn $ "done. (" ++ show iterations ++ " iterations, " ++ show (milliseconds start end) ++ "msec)"
+  where milliseconds start end = fromIntegral (toNanoSecs (diffTimeSpec start end)) / 10^6
+
+-- iei
